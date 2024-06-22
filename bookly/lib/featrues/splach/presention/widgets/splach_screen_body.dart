94fb90f34450c42e8d45e0f6341b8e96ce8395a2
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-06-22 04:03:57 +0300

    adding animation

--- bookly/lib/featrues/splach/presention/widgets/splach_screen_body.dart
+++ bookly/lib/featrues/splach/presention/widgets/splach_screen_body.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/featrues/splach/presention/widgets/sliding_text.dart';
 
-class SplachScreenBody extends StatelessWidget {
+class SplachScreenBody extends StatefulWidget {
   const SplachScreenBody({super.key});
+
+  @override
+  State<SplachScreenBody> createState() => _SplachScreenBodyState();
+}
+
+class _SplachScreenBodyState extends State<SplachScreenBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    slidingAnimation = Tween<Offset>(begin: Offset(0, 2), end: Offset.zero)
+        .animate(animationController);
+    animationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [Image.asset(AssetData.logo)],
+      children: [
+        Image.asset(AssetData.logo),
+        const SizedBox(
+          height: 5,
+        ),
+        SlidingText(slidingAnimation: slidingAnimation)
+      ],
     );
   }
 }
+
