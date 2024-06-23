900aa6722cea9c9d5c53dd9ad3c208c31b73780d
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-06-23 05:10:56 +0300

    create home screen

--- bookly/lib/featrues/home/presentation/screens/home_screen.dart
+++ bookly/lib/featrues/home/presentation/screens/home_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:bookly/featrues/home/presentation/screens/widgets/home_screen_body.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: HomeScreenBody(),
+    );
   }
 }
