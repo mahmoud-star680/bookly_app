bec77e4f693d8636de0930962cf4a77168a940cf
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-06-25 01:48:55 +0300

    create the home app bar

--- bookly/lib/featrues/home/presentation/screens/widgets/home_app_bar.dart
+++ bookly/lib/featrues/home/presentation/screens/widgets/home_app_bar.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:bookly/core/utils/assets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class HomeAppBar extends StatelessWidget {
+  const HomeAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      children: [],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 40),
+      child: Row(
+        children: [
+          Image.asset(
+            AssetData.logo,
+            height: 18,
+          ),
+          const SizedBox(
+            width: 50,
+          ),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.magnifyingGlass,
+                size: 24,
+              )),
+        ],
+      ),
     );
   }
 }
--- bookly/lib/featrues/home/presentation/screens/widgets/home_screen_body.dart
+++ bookly/lib/featrues/home/presentation/screens/widgets/home_screen_body.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:bookly/featrues/home/presentation/screens/widgets/home_app_bar.dart';
 
 class HomeScreenBody extends StatelessWidget {
   const HomeScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Column(
+      children: [
+        HomeAppBar(),
+      ],
+    );
   }
 }
