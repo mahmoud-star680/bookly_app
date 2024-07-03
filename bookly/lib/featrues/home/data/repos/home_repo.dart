e5b35a27f314317a329a78c7c7559a122897dee7
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-07-03 05:08:24 +0300

    create failures file and add either to repo

--- bookly/lib/featrues/home/data/repos/home_repo.dart
+++ bookly/lib/featrues/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/featrues/home/data/Models/BookModel/book_model.dart';
+
 abstract class HomeRepo {
-  fetchFeatruedBooks();
-  fetchNewstBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeatruedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchNewstBooks();
 }
