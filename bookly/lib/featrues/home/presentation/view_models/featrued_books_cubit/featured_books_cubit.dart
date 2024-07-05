2d1954785af81878e858ab0409e5397b00ceb2a1
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-07-05 05:22:33 +0300

    start in logic

--- bookly/lib/featrues/home/presentation/view_models/featrued_books_cubit/featured_books_cubit.dart
+++ bookly/lib/featrues/home/presentation/view_models/featrued_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/featrues/home/data/repos/home_repo.dart';
 import 'package:bookly/featrues/home/data/Models/BookModel/book_model.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeatruedBooks() async {
+    emit(FeaturedBooksLoading());
+
+    var result = await homeRepo.fetchFeatruedBooks();
+
+    result.fold((Failure) {
+      emit(FeaturedBooksFailure(Failure.errMessage));
+    }, (books) {
+      emit(FeaturedBooksSuccess(books));
+    });
+  }
 }
